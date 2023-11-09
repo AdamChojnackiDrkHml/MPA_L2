b9d996e00e52f7a5844334c447cfaced2e231345
AdamChojnackiDrkHml <adam.chojnacki.syl@gmail.com>
2023-11-09 08:34:21 +0100

    Plots

--- plotter.py
+++ plotter.py
@@ -2,17 +2,22 @@ import numpy as np
 from pandas import read_csv
 from matplotlib import pyplot as plt
 
-arr = np.loadtxt("data/resultTest.csv", delimiter=";")
-arr = arr.transpose()
-X = range(100, 1000, 25)
+def plot(path, suff):
+    arr = np.loadtxt(path, delimiter=";")
+    arr = arr.transpose()
+    X = range(200, 5000, 40)
 
-plt.plot(X, arr[0], label="Kruskal")
-plt.plot(X, arr[1], label="Prim")
-plt.xlabel("N")
-plt.ylabel("Time")
-plt.legend()
-plt.savefig("plots/Comp")
-plt.clf()
+    plt.plot(X, arr[0], label="Kruskal")
+    plt.plot(X, arr[1], label="Prim")
+    plt.xlabel("N")
+    plt.ylabel("Time")
+    plt.legend()
+    plt.savefig(f"plots/Comp_{suff}")
+    plt.clf()
 
-plt.plot(X, arr[2])
-plt.savefig("plots/MST")
+    plt.plot(X, arr[2])
+    plt.savefig(f"plots/MST_{suff}")
+    plt.clf()
+    
+plot("data/resultTestUni.csv", "Uni")
+plot("data/resultTestNorm.csv", "Norm")
