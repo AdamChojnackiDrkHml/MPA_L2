fb245f42b953bbdbe3f90e63f78a32322a19b8a5
AdamChojnackiDrkHml <adam.chojnacki.syl@gmail.com>
2023-12-07 23:36:30 +0100

    All Done L3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{time::Instant, fs::File, io::Write};
+use rand::{seq::IteratorRandom, distributions::Uniform, Rng, thread_rng};
 
 
 use graph::{Edge, Graph};
@@ -9,13 +10,14 @@ pub mod prim;
 pub mod propagator;
 
 fn main() {
-    // let min_n = 200;
-    // let max_n = 5000;
-    // let step = 40;
-    // let rep = 100;
+    let min_n = 100;
+    let max_n = 10100;
+    let step = 500;
+    let rep = 30;
+    let rep_root = 30;
 
-    // let mut file_uni = File::create("data/resultTestUni.csv").unwrap();
-    // let mut file_norm = File::create("data/resultTestNorm.csv").unwrap();
+    let mut file_uni = File::create("data/dataL3/resultTestUniFin2.csv").unwrap();
+    let mut file_norm = File::create("data/dataL3/resultTestNormFin2.csv").unwrap();
     
     // for n in (min_n..max_n).step_by(step) {
     //     let res_uni = single_test(n, rep, graph::Graph::create_full_graph_uniform);
@@ -27,9 +29,20 @@ fn main() {
     //     write_res_to_file(&mut file_norm, &res_norm, n);
     // }
 
-    let g = Graph::create_full_graph_uniform(10);
-    let mst = kruskal::kruskal(&g);
-    test_propagator(&mst)
+    file_uni.write_all("n;rounds;time\n".as_bytes()).unwrap();
+    file_norm.write_all("n;rounds;time\n".as_bytes()).unwrap();
+
+    for n in (min_n..max_n).step_by(step) {
+        let res_uni = single_test_propagator(n, rep, rep_root, graph::Graph::create_full_graph_uniform);
+
+        write_res_to_file_propagator(&mut file_uni, &res_uni, n);
+
+        let res_norm = single_test_propagator(n, rep, rep_root, graph::Graph::create_full_graph_normal);
+
+        write_res_to_file_propagator(&mut file_norm, &res_norm, n);
+
+        println!("Done {}", n)
+    }
 }
 
 fn sum_edges(mst: &Vec<Edge>) -> f64 {
@@ -64,7 +77,37 @@ fn write_res_to_file(file: &mut File, res: &Vec<(f64, f64, f64)>, n: usize) {
     file.write_all((str_vec.join("\n") + "\n").as_bytes()).unwrap();
 }
 
-fn test_propagator(mst: &Vec<Edge>) {
-    println!("{:?}", mst);
-    println!("{}", propagator::propagate(mst, 0));
+fn write_res_to_file_propagator(file: &mut File, res: &Vec<(usize, f64)>, n: usize) {
+    let str_vec: Vec<String> = res.into_iter().map(|(r, t)| format!("{};{};{}", n, r, t)).collect();
+
+    file.write_all((str_vec.join("\n") + "\n").as_bytes()).unwrap();
+}
+
+fn test_propagator(mst: &Vec<Edge>) -> usize {
+    let rng = rand::thread_rng().gen_range(0..mst.len() - 1);
+    propagator::propagate(mst, rng)
+}
+
+fn average(numbers: &Vec<usize>) -> f32 {
+    numbers.iter().sum::<usize>() as f32 / numbers.len() as f32
+}
+
+fn single_test_propagator(n: usize, rep: usize, rep_root: usize, generator: impl Fn(usize) -> graph::Graph) -> Vec<(usize, f64)> {
+    let mut res: Vec<(usize, f64)> = vec![];
+
+    for _ in 0..rep {
+        let g = generator(n);
+        let mst = prim::prim(&g);
+        let mut rng = thread_rng();
+        assert!(rep_root <= &mst.len() - 1);
+        let sample = (0 as usize..&mst.len()-1).into_iter().choose_multiple(&mut rng, rep_root);
+        for root in sample.into_iter() {
+            let start_time = Instant::now();
+            let rounds = propagator::propagate(&mst, root);
+            let total_time = start_time.elapsed().as_secs() as f64 + start_time.elapsed().subsec_nanos() as f64 / 1e9;
+            res.push((rounds, total_time));
+        }
+    }
+
+    return res;
 }
--- src/propagator.rs
+++ src/propagator.rs
@@ -39,38 +39,34 @@ fn create_rooted_node(tree_map: &HashMap<usize, Vec<usize>>, me: usize, parent: 
         .filter(|x| *x != parent)
         .collect();
 
-    print!("Imgere");
-
     let subtrees = subtrees_indexes
         .into_iter()
         .map(|x| create_rooted_node(tree_map, x, me))
         .collect();
 
-    println!("{:?}", subtrees);
-
-    let mut roooted_node = PropagatorNode{
+    let mut rooted_node = PropagatorNode{
         max_depth: 0,
         subtrees,
     };  
 
-    if roooted_node.subtrees.len() == 0 {
-        roooted_node.max_depth = 1;
+    if rooted_node.subtrees.len() == 0 {
+        rooted_node.max_depth = 0;
     } else {
-        roooted_node.max_depth = 1 + roooted_node.subtrees
+        rooted_node.max_depth = 1 + rooted_node.subtrees
         .iter()
         .map(|node| node.max_depth)
         .max()
         .unwrap();
 
-        roooted_node.subtrees.sort_by_key(|a| Reverse(a.max_depth));
+        rooted_node.subtrees.sort_by_key(|a| Reverse(a.max_depth));
     }
 
-    roooted_node
+    rooted_node
 }
 
 pub fn propagate(mst: &Vec<Edge>, root: usize) -> usize {
     let rooted_tree = create_rooted_tree(&construct_adj_map(mst), root);
-    println!("{:?}", rooted_tree);
+    // println!("{:?}", rooted_tree);
     propagate_node(&rooted_tree)
 }
 
@@ -78,17 +74,21 @@ fn propagate_node(p_node: &PropagatorNode) -> usize {
     let mut round = 1;
     let mut rounds: Vec<usize> = Vec::new();
 
-    
-
     for s_tree in p_node.subtrees.iter() {
         let rounds_in_node = propagate_node(&s_tree); 
         rounds.push(rounds_in_node + round);
         round += 1;
     }
+    
+    let number_of_subtrees = p_node.subtrees.len();
 
-    print!("ADFSA");
-    match rounds.len() {
+    match number_of_subtrees {
         0 => 1,
-        _ => *rounds.iter_mut().max().unwrap(), 
+        _ => rounds
+            .iter()
+            .enumerate()
+            .map(|(i, x)| *x+i)
+            .max()
+            .unwrap(), 
     }
 }
